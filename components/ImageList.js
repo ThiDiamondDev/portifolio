f18cdf9d29f8f715a3757189bab131a7eed450ee
ThiDiamondDev <thi.dimond1@gmail.com>
2023-07-03 15:14:55 -0300

    Extract ImageItem to function to remove key warnin

--- components/ImageList.js
+++ components/ImageList.js
@@ -10,21 +10,23 @@ const itemData = [
   { img: "/PortfolioImages/Taurus.png", title: "Minotaurus" },
 ];
 
+const ImageItem = (item, index, onClick) => (
+  <a key={index} onClick={() => onClick(item)}>
+    <ImageListItem>
+      <img
+        src={`${item.img}?w=248&fit=crop&auto=format`}
+        srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+        alt={item.title}
+        loading="lazy"
+      />
+    </ImageListItem>
+  </a>
+);
+
 export default function MyImageList({ onClick }) {
   return (
     <ImageList variant="masonry" cols={3} gap={8}>
-      {itemData.map((item) => (
-        <a onClick={() => onClick(item)}>
-          <ImageListItem key={item.img}>
-            <img
-              src={`${item.img}?w=248&fit=crop&auto=format`}
-              srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
-              alt={item.title}
-              loading="lazy"
-            />
-          </ImageListItem>
-        </a>
-      ))}
+      {itemData.map((item, index) => ImageItem(item, index, onClick))}
     </ImageList>
   );
 }
