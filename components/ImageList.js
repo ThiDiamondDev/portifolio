8cd0557d5bee4c0d42ffd71d47d9bea00907965f
ThiDiamondDev <thi.dimond1@gmail.com>
2023-07-03 14:45:33 -0300

    Add guitar to ImageList

--- components/ImageList.js
+++ components/ImageList.js
@@ -1,12 +1,12 @@
 import { ImageList, ImageListItem } from "@mui/material";
 const itemData = [
+  { img: "/PortfolioImages/guitar.png", title: "8 Strings Guitar" },
   { img: "/PortfolioImages/church2.png", title: "Old Country Church" },
-
   { img: "/PortfolioImages/CLOCK.png", title: "Old Clock" },
   { img: "/PortfolioImages/revolver.png", title: "Revolver" },
-  { img: "/PortfolioImages/scyth.png", title: "Death Scythe" },
   { img: "/PortfolioImages/Monster.png", title: "Horror Creature" },
   { img: "/PortfolioImages/art.png", title: "Art the Clown from Terrifier" },
+  { img: "/PortfolioImages/scyth.png", title: "Death Scythe" },
   { img: "/PortfolioImages/Taurus.png", title: "Minotaurus" },
 ];
 
