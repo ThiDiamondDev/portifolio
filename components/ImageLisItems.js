8d0580baa4927b943f8985c99b8f58eaa7e434e6
ThiDiamondDev <thi.dimond1@gmail.com>
2023-07-20 12:40:25 -0300

    Add monster retopo image

--- components/ImageLisItems.js
+++ components/ImageLisItems.js
@@ -5,13 +5,14 @@ const itemData = [
 
   { img: "DonnieDarkoCompressed.png", title: "Donnie Darko" },
   { img: "guitar.png", title: "8 Strings Guitar" },
+  { img: "monsterretopo.png", title: "Monster Retopology" },
 
   { img: "revolver.png", title: "Revolver" },
 
   { img: "CHURCH-INSIDE.png", title: "Old Church Interior" },
 
-  { img: "Monster.png", title: "Horror Creature" },
   { img: "art.png", title: "Art the Clown from Terrifier" },
+  { img: "Monster.png", title: "Horror Creature" },
 
   { img: "scyth.png", title: "Death Scythe" },
   { img: "Taurus.png", title: "Minotaurus" },
